82439ce1eade0fe9198ae709704f6fbf1eea4daf
Piotr Kowalski <piecioshka@gmail.com>
2019-05-15 12:19:05 +0200

    Conver Arrow Functions to Function Definition

--- test/slice.test.js
+++ test/slice.test.js
@@ -1,27 +1,27 @@
-describe('slice', () => {
+describe('slice', function () {
 
-    it('should not mutate an array', () => {
+    it('should not mutate an array', function () {
         const input = ['a', 'b', 'c', 'd', 'e'];
         input.slice(1);
         const output = ['a', 'b', 'c', 'd', 'e'];
         expect(input).toEqual(output);
     });
 
-    it('should not mutate array with params', () => {
+    it('should not mutate array with params', function () {
         const input = ['a', 'b', 'c', 'd', 'e'];
         input.slice(1, 2);
         const output = ['a', 'b', 'c', 'd', 'e'];
         expect(input).toEqual(output);
     });
 
-    it('should return new array', () => {
+    it('should return new array', function () {
         const list = ['a', 'b', 'c', 'd', 'e'];
         const input = list.slice(1, 2);
         const output = ['b'];
         expect(input).toEqual(output);
     });
 
-    it('should copy an array', () => {
+    it('should copy an array', function () {
         const list = ['a', 'b', 'c', 'd', 'e'];
         const input = list.slice();
         const output = ['a', 'b', 'c', 'd', 'e'];
--- test/splice.test.js
+++ test/splice.test.js
@@ -1,20 +1,20 @@
-describe('splice', () => {
+describe('splice', function () {
 
-    it('should mutate an array', () => {
+    it('should mutate an array', function () {
         const input = ['a', 'b', 'c', 'd', 'e'];
         input.splice(1);
         const output = ['a'];
         expect(input).toEqual(output);
     });
 
-    it('should mutate an array with param', () => {
+    it('should mutate an array with param', function () {
         const input = ['a', 'b', 'c', 'd', 'e'];
         input.splice(1, 2);
         const output = ['a', 'd', 'e'];
         expect(input).toEqual(output);
     });
 
-    it('should return cut part', () => {
+    it('should return cut part', function () {
         const list = ['a', 'b', 'c', 'd', 'e'];
         const input = list.splice(1, 2);
         const output = ['b', 'c'];
